3ac9afa32374e794e7d889fc7d3c2efa520ccf96
Aditya_Prasad <adityaprasad186@gmail.com>
2024-12-18 08:09:26 +0530

    first commit

---
+++ src/sections/Footer.tsx
@@ -0,0 +1,57 @@
+import Logo from "@/assets/logo.svg";
+import YoutubeSocial from "@/assets/social-youtube.svg";
+import XSocial from "@/assets/social-x.svg";
+import InstaSocial from "@/assets/social-instagram.svg";
+export const Footer = () => {
+  return (
+    <footer className="py-5 border-t border-white/15">
+      <div className="container">
+        <div className="flex flex-col gap-8 lg:flex-row lg:items-center">
+          <div className="flex gap-2 items-center lg:flex-1">
+            <Logo className="h-6 w-6" />
+            <div className="font-medium">AI Startup Landing Page</div>
+          </div>
+
+          <nav className="flex flex-col lg:flex-row lg:gap-7 gap-5 lg:flex-1 lg:justify-center">
+            <a
+              href=""
+              className="text-white/70 hover:text-white text-xs md:text-sm"
+            >
+              Features
+            </a>
+            <a
+              href=""
+              className="text-white/70 hover:text-white text-xs md:text-sm"
+            >
+              Developers
+            </a>
+            <a
+              href=""
+              className="text-white/70 hover:text-white text-xs md:text-sm"
+            >
+              Company
+            </a>
+            <a
+              href=""
+              className="text-white/70 hover:text-white text-xs md:text-sm"
+            >
+              Blog
+            </a>
+            <a
+              href=""
+              className="text-white/70 hover:text-white text-xs md:text-sm"
+            >
+              ChangeLog
+            </a>
+          </nav>
+
+          <div className="flex gap-5 lg:flex-1 lg:justify-end">
+            <XSocial className="text-white/40 hover:text-white transition" />
+            <YoutubeSocial className="text-white/40 hover:text-white transition" />
+            <InstaSocial className="text-white/40 hover:text-white transition" />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
